bb5cfcf38e1fdf1bc7f2ffea87b9734d8b312821
Gaston Messeri <gastonmesseri@gmail.com>
2020-12-28 03:57:37 +0100

    Update title and description in docs

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -63,8 +63,8 @@ function Home() {
     const { siteConfig = {} } = context;
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />"
+            title={`${siteConfig.title}`}
+            description="Number formatting library for Javascript and Node.js apps"
         >
             <header className={clsx('hero hero--primary', styles.heroBanner)}>
                 <div className="container">
