a71585d51e743036d97334b0e23eb2a56deef564
Gaston Messeri <gastonmesseri@gmail.com>
2020-12-29 20:25:01 +0100

    Refactor numberToNonExponentialString

--- src/core/utils/number-to-non-exponential-string.ts
+++ src/core/utils/number-to-non-exponential-string.ts
@@ -3,7 +3,6 @@ import splitStringInTwoParts from './split-string-in-two-parts';
 
 /**
  * The result from toFixed can contain an exponent for big numbers (e.g. 1.12345671234567e+50).
- * So far, only for positive exponents.
  * <!> Only handles positive exponents.
  */
 const formatPositiveExponentResult = (valueAsString: string) => {
@@ -14,6 +13,25 @@ const formatPositiveExponentResult = (valueAsString: string) => {
     const [integerPartOfSignificand, fractionalPartOfSignificand] = splitStringInTwoParts(significand, '.');
     const numberOfZerosToAdd = exponentAsNumber - fractionalPartOfSignificand.length;
     return `${integerPartOfSignificand}${fractionalPartOfSignificand}${stringRepeat('0', numberOfZerosToAdd)}`;
+};
+
+/**
+ * The result from toFixed can contain an exponent for small numbers (e.g. 1.123e-87).
+ * <i> Only handles negative exponents
+ */
+const formatNegativeExponentResult = (value: number, exponentAsNumber: number, significandAsString: string) => {
+    const negativeExponentAbsoluteValue = Math.abs(exponentAsNumber);
+    const [integerPartOfSignificand, fractionalPartOfSignificand] = splitStringInTwoParts(significandAsString, '.');
+    const absoluteIntegerPartOfSignificand = integerPartOfSignificand[0] === '-' ? integerPartOfSignificand.slice(1) : integerPartOfSignificand;
+    let outputIntegerPartOfSignificand = absoluteIntegerPartOfSignificand;
+    let outputFractionalPartOfSignificand = fractionalPartOfSignificand;
+    for (let i = 0; i < negativeExponentAbsoluteValue; i += 1) {
+        // Consider using array.shift
+        const firstCharInIntegerPart = outputIntegerPartOfSignificand[0] || '';
+        outputIntegerPartOfSignificand = outputIntegerPartOfSignificand.slice(0, outputIntegerPartOfSignificand.length - 1);
+        outputFractionalPartOfSignificand = (firstCharInIntegerPart || '0') + outputFractionalPartOfSignificand;
+    }
+    return `${value < 0 ? '-' : ''}${outputIntegerPartOfSignificand || 0}.${outputFractionalPartOfSignificand}`;
 };
 
 /**
@@ -30,37 +48,16 @@ const formatPositiveExponentResult = (valueAsString: string) => {
  */
 const numberToNonExponentialString = (value: number) => {
     const valueAsString = (value || 0).toString();
-
-    // Do it here all the process
     const valueAsStringHasExponentialInfo = valueAsString.indexOf('e') >= 0;
 
     if (!valueAsStringHasExponentialInfo) return valueAsString;
 
-    // If the toString returns an exponential number
+    // If the toString returns an exponential number (e.g. 1.23e+28)
     const [significand, exponent] = splitStringInTwoParts(valueAsString, 'e');
     const exponentAsNumber = +exponent;
-    let output = valueAsString;
-
-    if (exponentAsNumber >= 0) {
-        // Positive exponents (big numbers)
-        output = formatPositiveExponentResult(output);
-    } else {
-        // Negative exponents (small numbers) only equal or smaller than -1
-        const negativeExponentAbsoluteValue = Math.abs(exponentAsNumber);
-        const [integerPartOfSignificand, fractionalPartOfSignificand] = splitStringInTwoParts(significand, '.');
-        const absoluteIntegerPartOfSignificand = integerPartOfSignificand[0] === '-' ? integerPartOfSignificand.slice(1) : integerPartOfSignificand;
-        let outputIntegerPartOfSignificand = absoluteIntegerPartOfSignificand;
-        let outputFractionalPartOfSignificand = fractionalPartOfSignificand;
-        for (let i = 0; i < negativeExponentAbsoluteValue; i += 1) {
-            // Consider using array.shift
-            const firstCharInIntegerPart = outputIntegerPartOfSignificand[0] || '';
-            outputIntegerPartOfSignificand = outputIntegerPartOfSignificand.slice(0, outputIntegerPartOfSignificand.length - 1);
-            outputFractionalPartOfSignificand = (firstCharInIntegerPart || '0') + outputFractionalPartOfSignificand;
-        }
-        output = `${value < 0 ? '-' : ''}${outputIntegerPartOfSignificand || 0}.${outputFractionalPartOfSignificand}`;
-    }
-
-    return output;
+    return exponentAsNumber >= 0
+        ? formatPositiveExponentResult(valueAsString)
+        : formatNegativeExponentResult(value, exponentAsNumber, significand);
 };
 
 export default numberToNonExponentialString;
